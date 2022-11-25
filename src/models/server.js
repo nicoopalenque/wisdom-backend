04a67ee22ba95b464bd25f329e8fb0994e2108fa
Nicolas Palenque <nicoopalenque@gmail.com>
2022-11-25 18:53:56 -0300

    adding cors

--- src/models/server.js
+++ src/models/server.js
@@ -28,7 +28,7 @@ class Server{
 
     middlewares(){
         const allowCrossDomain = function(req, res, next) {
-            res.header('Access-Control-Allow-Origin', "http://localhost:3000");
+            res.header('Access-Control-Allow-Origin', "*");
             res.header('Access-Control-Allow-Methods', '*');
             res.header('Access-Control-Allow-Headers', '*');
             next();
